3d21c57da3f109ee2273fc2ba153ba5056db6219
antonioprog23 <126322049+antonioprog23@users.noreply.github.com>
2023-03-24 22:34:56 +0100

    COMPRA_ANTONIO

---
+++ ProyectoCompra/Controles/TextBoxTipo.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace ProyectoCompra.Controles
+{
+    partial class TextBoxTipo
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(0, 0);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(180, 20);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // TextBoxTipo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox);
+            this.Name = "TextBoxTipo";
+            this.Size = new System.Drawing.Size(180, 21);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
---
+++ ProyectoCompra/Controles/TextBoxTipo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ProyectoCompra.Controles
+{
+    public partial class TextBoxTipo : UserControl
+    {
+        private bool isNumerico;
+        public TextBoxTipo()
+        {
+            InitializeComponent();
+        }
+
+        [Browsable(true)]
+        [Category("Tipo TextBox")]
+        public bool IsNumerico { get => isNumerico; set => isNumerico = value; }
+        protected override void OnLayout(LayoutEventArgs levent)
+        {
+            base.OnLayout(levent);
+
+            // Ajustar el tamaño y la posición del control en relación con el formulario contenedor
+            this.Width = this.Parent.ClientSize.Width;
+            this.Height = this.Parent.ClientSize.Height;
+            this.Location = new Point(0, 0);
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (isNumerico)
+            {
+                Regex regex = new Regex("[^0-9]");
+                textBox.Text = regex.Replace(textBox.Text, "");
+                textBox.Select(textBox.Text.Length, 0);
+            }
+            else
+            {
+                Regex regex = new Regex("[^a-zA-Z]");
+                textBox.Text = regex.Replace(textBox.Text, "");
+                textBox.Select(textBox.Text.Length, 0);
+            }
+        }
+    }
+
+
+
+
+}
