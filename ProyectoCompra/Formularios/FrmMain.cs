24d774d9e4fcb806ed493b8110992f9dd4d8992c
antonioprog23 <126322049+antonioprog23@users.noreply.github.com>
2023-04-02 15:34:47 +0200

    COMPRA_ANTONIO_DATOS_BD

--- ProyectoCompra/Formularios/FrmMain.cs
+++ ProyectoCompra/Formularios/FrmMain.cs
@@ -51,283 +51,283 @@ namespace ProyectoCompra.Formularios
 
         private void btnVerdurasHortalizas_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(2, 1);
+            FrmProductos frmProductos = new FrmProductos(2, 7);
             frmProductos.ShowDialog();
         }
 
         private void btnQuesos_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(2, 2);
+            FrmProductos frmProductos = new FrmProductos(2, 8);
             frmProductos.ShowDialog();
         }
 
         private void btnCarne_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(2, 3);
+            FrmProductos frmProductos = new FrmProductos(2, 9);
             frmProductos.ShowDialog();
         }
 
         private void btnCharcuteria_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(2, 4);
+            FrmProductos frmProductos = new FrmProductos(2, 10);
             frmProductos.ShowDialog();
         }
 
         private void btnPescados_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(2, 5);
+            FrmProductos frmProductos = new FrmProductos(2, 11);
             frmProductos.ShowDialog();
         }
 
         private void btnSushi_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(2, 6);
+            FrmProductos frmProductos = new FrmProductos(2, 12);
             frmProductos.ShowDialog();
         }
 
         private void btnCongelados_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(3, 1);
+            FrmProductos frmProductos = new FrmProductos(3, 13);
             frmProductos.ShowDialog();
         }
 
         private void btnPostres_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(3, 2);
+            FrmProductos frmProductos = new FrmProductos(3, 14);
             frmProductos.ShowDialog();
         }
 
         private void btnPasteles_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(3, 3);
+            FrmProductos frmProductos = new FrmProductos(3, 15);
             frmProductos.ShowDialog();
         }
 
         private void btnMantequillas_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(3, 4);
+            FrmProductos frmProductos = new FrmProductos(3, 16);
             frmProductos.ShowDialog();
         }
 
         private void btnPlatosListos_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(3, 5);
+            FrmProductos frmProductos = new FrmProductos(3, 17);
             frmProductos.ShowDialog();
         }
 
         private void btnPastas_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(3, 6);
+            FrmProductos frmProductos = new FrmProductos(3, 18);
             frmProductos.ShowDialog();
         }
 
         private void btnZumos_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(4, 1);
+            FrmProductos frmProductos = new FrmProductos(4, 19);
             frmProductos.ShowDialog();
         }
 
         private void btnAguaSoda_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(4, 2);
+            FrmProductos frmProductos = new FrmProductos(4, 20);
             frmProductos.ShowDialog();
         }
 
         private void btnBebidaAlcohol_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(4, 3);
+            FrmProductos frmProductos = new FrmProductos(4, 21);
             frmProductos.ShowDialog();
         }
 
         private void btnCavaSidra_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(4, 4);
+            FrmProductos frmProductos = new FrmProductos(4, 22);
             frmProductos.ShowDialog();
         }
 
         private void btnBebidaEcologica_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(4, 5);
+            FrmProductos frmProductos = new FrmProductos(4, 23);
             frmProductos.ShowDialog();
         }
 
         private void btnSinAlcohol_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(4, 6);
+            FrmProductos frmProductos = new FrmProductos(4, 24);
             frmProductos.ShowDialog();
         }
 
         private void btnLavadoRopa_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(5, 1);
+            FrmProductos frmProductos = new FrmProductos(5, 25);
             frmProductos.ShowDialog();
         }
 
         private void btnLimpiezaHogar_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(5, 2);
+            FrmProductos frmProductos = new FrmProductos(5, 26);
             frmProductos.ShowDialog();
         }
 
         private void btnLavavajillas_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(5, 3);
+            FrmProductos frmProductos = new FrmProductos(5, 27);
             frmProductos.ShowDialog();
         }
 
         private void btnLejiasAmoniacos_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(5, 4);
+            FrmProductos frmProductos = new FrmProductos(5, 28);
             frmProductos.ShowDialog();
         }
 
         private void btnInsecticidas_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(5, 5);
+            FrmProductos frmProductos = new FrmProductos(5, 29);
             frmProductos.ShowDialog();
         }
 
         private void btnLimpiezaCalzado_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(5, 6);
+            FrmProductos frmProductos = new FrmProductos(5, 30);
             frmProductos.ShowDialog();
         }
 
         private void btnPerfumeria_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(6, 1);
+            FrmProductos frmProductos = new FrmProductos(6, 31);
             frmProductos.ShowDialog();
         }
 
         private void btnBucal_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(6, 2);
+            FrmProductos frmProductos = new FrmProductos(6, 32);
             frmProductos.ShowDialog();
         }
 
         private void btnBotiquin_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(6, 3);
+            FrmProductos frmProductos = new FrmProductos(6, 33);
             frmProductos.ShowDialog();
         }
 
         private void btnCuidadoFacial_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(6, 4);
+            FrmProductos frmProductos = new FrmProductos(6, 34);
             frmProductos.ShowDialog();
         }
 
         private void btnCuidadoCabello_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(6, 5);
+            FrmProductos frmProductos = new FrmProductos(6, 35);
             frmProductos.ShowDialog();
         }
 
         private void btnLocionSolar_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(6, 6);
+            FrmProductos frmProductos = new FrmProductos(6, 36);
             frmProductos.ShowDialog();
         }
 
         private void btnLavadora_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(7, 1);
+            FrmProductos frmProductos = new FrmProductos(7, 37);
             frmProductos.ShowDialog();
         }
 
         private void btnFrigorificos_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(7, 2);
+            FrmProductos frmProductos = new FrmProductos(7, 38);
             frmProductos.ShowDialog();
         }
 
         private void btnSecadora_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(7, 3);
+            FrmProductos frmProductos = new FrmProductos(7, 39);
             frmProductos.ShowDialog();
         }
 
         private void btnHornoPlaca_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(7, 4);
+            FrmProductos frmProductos = new FrmProductos(7, 40);
             frmProductos.ShowDialog();
         }
 
         private void btnCafeteras_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(7, 5);
+            FrmProductos frmProductos = new FrmProductos(7, 41);
             frmProductos.ShowDialog();
         }
 
         private void btnClimatizacion_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(7, 6);
+            FrmProductos frmProductos = new FrmProductos(7, 42);
             frmProductos.ShowDialog();
         }
 
         private void btnTelevisores_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(8, 1);
+            FrmProductos frmProductos = new FrmProductos(8, 43);
             frmProductos.ShowDialog();
         }
 
         private void btnTelefonia_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(8, 2);
+            FrmProductos frmProductos = new FrmProductos(8, 44);
             frmProductos.ShowDialog();
         }
 
         private void btnFotografia_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(8, 3);
+            FrmProductos frmProductos = new FrmProductos(8, 45);
             frmProductos.ShowDialog();
         }
 
         private void btnInformatica_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(8, 4);
+            FrmProductos frmProductos = new FrmProductos(8, 46);
             frmProductos.ShowDialog();
         }
 
         private void btnTablets_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(8, 5);
+            FrmProductos frmProductos = new FrmProductos(8, 47);
             frmProductos.ShowDialog();
         }
 
         private void btnAudio_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(8, 6);
+            FrmProductos frmProductos = new FrmProductos(8, 48);
             frmProductos.ShowDialog();
         }
 
         private void btnJardin_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(9, 1);
+            FrmProductos frmProductos = new FrmProductos(9, 49);
             frmProductos.ShowDialog();
         }
 
         private void btnBricolaje_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(9, 2);
+            FrmProductos frmProductos = new FrmProductos(9, 50);
             frmProductos.ShowDialog();
         }
 
         private void btnMascotas_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(9, 3);
+            FrmProductos frmProductos = new FrmProductos(9, 51);
             frmProductos.ShowDialog();
         }
 
         private void btnCasaDecoracion_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(9, 4);
+            FrmProductos frmProductos = new FrmProductos(9, 52);
             frmProductos.ShowDialog();
         }
 
         private void btnTextilHogar_Click(object sender, EventArgs e)
         {
-            FrmProductos frmProductos = new FrmProductos(9, 5);
+            FrmProductos frmProductos = new FrmProductos(9, 53);
             frmProductos.ShowDialog();
         }
     }
--- ProyectoCompra/Properties/AlimentacionS2.Designer.cs
+++ ProyectoCompra/Properties/AlimentacionS2.Designer.cs
@@ -59,15 +59,5 @@ namespace ProyectoCompra.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _2_1 {
-            get {
-                object obj = ResourceManager.GetObject("_2_1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
