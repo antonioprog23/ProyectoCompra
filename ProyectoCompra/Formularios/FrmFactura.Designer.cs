3d11ec8178790d34f07641762abd12e6b1c15395
antonioprog23 <126322049+antonioprog23@users.noreply.github.com>
2023-05-25 00:42:15 +0200

    COMPRA_ANTONIO_REPORTE

--- ProyectoCompra/Formularios/FrmFactura.Designer.cs
+++ ProyectoCompra/Formularios/FrmFactura.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.ConsultarFacturaBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.dSFactura = new ProyectoCompra.Reportes.DSFactura();
             this.dSFacturaBindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -69,21 +69,21 @@
             // reportViewer1
             // 
             this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            reportDataSource2.Name = "DSFactura";
-            reportDataSource2.Value = this.ConsultarFacturaBindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
+            reportDataSource1.Name = "DSFactura";
+            reportDataSource1.Value = this.ConsultarFacturaBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
             this.reportViewer1.LocalReport.ReportEmbeddedResource = "ProyectoCompra.Reportes.InformeFactura.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.reportViewer1.Size = new System.Drawing.Size(810, 450);
             this.reportViewer1.TabIndex = 0;
             // 
             // FrmFactura
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(810, 450);
             this.Controls.Add(this.reportViewer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "FrmFactura";
